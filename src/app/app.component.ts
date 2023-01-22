5d87e885080ff35e7963ffc19eeafa3194d2c9f6
kaslabarak702 <101200976+kaslabarak702@users.noreply.github.com>
2023-01-22 19:45:37 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './models/face-snap.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'face-snap';
+export class AppComponent implements OnInit {
+  mySnap1!:FaceSnap;
+  mySnap2!:FaceSnap;
+  mySnap3!:FaceSnap;
+  ngOnInit(): void {
+    this.mySnap1=new FaceSnap("archibald","mon meilleur ami","https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843__480.png",new Date(),100000);
+    this.mySnap2=new FaceSnap("francis","mon pire ami","https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843__480.png",new Date(),2);
+    this.mySnap3=new FaceSnap("jean","pas mon ami","https://cdn.pixabay.com/photo/2016/03/21/23/25/link-1271843__480.png",new Date(),400000000000);
+  }
 }
